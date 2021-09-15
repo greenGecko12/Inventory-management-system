efc4823c2e39d96eb964459b4dec426133fddf81
Sai Kommuru <sai.kommuru123@gmail.com>
2021-09-15 20:39:00 +0100

    Finished project - small issue with 'search item'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import SearchBar from "./SearchBar";
 import AddItem from "./AddItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ItemsDisplay from "./ItemsDisplay";
-import styled from "styled-components"; //have a look at this documentation
+//import styled from "styled-components"; //have a look at this documentation
 
 function App() {
   const [filters, setFilters] = useState({
@@ -12,23 +12,66 @@ function App() {
     type: "",
     brand: "",
   });
+
   const [data, setData] = useState({ items: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((response) => response.json())
+      .then((data) => setData({ items: data }));
+  }, []);
 
   const updateFilters = (searchParams) => {
     setFilters(searchParams);
   };
 
+  const deleteItem = (item) => {
+    const items = data.items;
+    const requestOptions = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:3000/items/${item.id}`, requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          const idx = items.indexOf(item);
+          items.splice(idx, 1);
+          setData({ items });
+        }
+      }
+    );
+  };
+
   const addItemtoData = (item) => {
     let items = data.items;
-    item.id = items.length;
-    items.push(item);
-    setData({ items });
 
-    console.log(data);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items });
+      });
   };
 
   const filterData = (data) => {
     const filteredData = [];
+
+    // if (
+    //   //if there are no filters
+    //   filters.name === "" &&
+    //   filters.price === 0 &&
+    //   filters.brand === "" &&
+    //   filters.type === ""
+    // ) {
+    //   return data;
+    // }
 
     if (!filters.name) {
       return data;
@@ -52,13 +95,13 @@ function App() {
       filteredData.push(item);
     }
 
-    return filterData;
+    return filteredData;
   };
 
   return (
     <div className="container">
       <div className="row mt-3">
-        <ItemsDisplay items={filterData(data.items)} />
+        <ItemsDisplay deleteItem={deleteItem} items={filterData(data.items)} />
       </div>
 
       <div className="row mt-3">
